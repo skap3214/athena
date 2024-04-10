94e2e55b13c843a13f2d887c280f55a951456ad1
achyutbenz19 <achyut.benz@gmail.com>
2024-04-10 14:23:44 -0400

    upload css

--- components/submit-area.tsx
+++ components/submit-area.tsx
@@ -2,7 +2,13 @@
 import { SubmitAreaProps } from "@/types";
 import { Textarea } from "./ui/textarea";
 import { checkInputType } from "@/lib/check-input-type";
-import { CaseUpper, ArrowRight, Paperclip } from "lucide-react";
+import {
+  CaseUpper,
+  ArrowRight,
+  Paperclip,
+  FileCheck,
+  Delete,
+} from "lucide-react";
 import { FaYoutube } from "react-icons/fa";
 import { Button } from "./ui/button";
 import { useState } from "react";
@@ -15,6 +21,7 @@ const SubmitArea = ({ handleSubmit, value, setValue }: SubmitAreaProps) => {
   const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       const selectedFile = event.target.files[0];
+      setValue("");
       setFile(selectedFile);
       console.log(selectedFile);
     }
@@ -39,10 +46,15 @@ const SubmitArea = ({ handleSubmit, value, setValue }: SubmitAreaProps) => {
     setIsDragOver(false);
   };
 
+  const handleClear = () => {
+    setFile(null);
+    setValue("");
+  };
+
   const labelText = isDragOver
     ? "Drop file here"
     : file
-      ? "File uploaded"
+      ? `${file.name} uploaded`
       : "Drop a file, type or input YouTube link";
 
   return (
@@ -59,6 +71,7 @@ const SubmitArea = ({ handleSubmit, value, setValue }: SubmitAreaProps) => {
           onDrop={handleDrop}
         >
           <Textarea
+            disabled={!!file}
             autoFocus
             className={cn(
               "flex-1",
@@ -69,7 +82,7 @@ const SubmitArea = ({ handleSubmit, value, setValue }: SubmitAreaProps) => {
             placeholder={labelText}
           />
           <div className="mt-1 flex w-full justify-between">
-            <div className="h-full flex items-center justify-center">
+            <div className="h-full flex items-center justify-center space-x-1">
               <label
                 htmlFor="file-upload"
                 className="flex space-x-1.5 h-7 rounded-md p-2 cursor-pointer bg-neutral-800 dark:bg-neutral-200 dark:hover:bg-neutral-100 text-primary-foreground hover:bg-neutral-800/90"
@@ -86,15 +99,28 @@ const SubmitArea = ({ handleSubmit, value, setValue }: SubmitAreaProps) => {
                   onChange={handleChange}
                 />
               </label>
+              <span
+                onClick={handleClear}
+                className={cn(
+                  "items-center justify-center space-x-1.5 h-7 rounded-md p-2 pl-1.5 cursor-pointer hover:bg-transparent/5 hover:dark:bg-transparent/50 hover:text-accent-foreground hidden text-sm overflow-visible",
+                  (value || file) && "flex",
+                )}
+              >
+                <Delete className="h-4 w-4 dark:text-neutral-200 text-neutral-700" />
+              </span>
             </div>
             <div className="flex space-x-1 mb-8">
               <div className="h-full flex items-center justify-center">
-                {value &&
+                {file ? (
+                  <FileCheck className="h-5 mt-0.5 w-5" />
+                ) : (
+                  value &&
                   (checkInputType(value) ? (
                     <FaYoutube className="h-5 mt-0.5 w-5" />
                   ) : (
                     <CaseUpper className="h-5 mt-0.5 w-5" />
-                  ))}
+                  ))
+                )}
               </div>
               <Button
                 size="sm"
