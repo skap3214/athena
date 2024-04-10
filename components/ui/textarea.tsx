7efed93be4a40b34750ace769989d5c0b2bbf41a
achyutbenz19 <achyut.benz@gmail.com>
2024-04-10 16:56:01 -0400

    textarea css

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
         className={cn(
-          "flex shadow-lg min-h-[100px] dark:text-black dark:placeholder-neutral-700 text-white w-full rounded-md border border-input dark:bg-neutral-100 bg-neutral-900 p-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-80",
+          "flex shadow-lg min-h-[100px] max-h-[300px] dark:text-black dark:placeholder-neutral-700 text-white w-full rounded-md border border-input dark:bg-neutral-100 bg-neutral-900 p-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-80",
           className,
         )}
         ref={ref}
