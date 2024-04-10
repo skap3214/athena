993d819c3028d8222283980f7ae394779dcfe128
achyutbenz19 <achyut.benz@gmail.com>
2024-04-10 09:00:47 -0400

    node click

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -1,13 +1,26 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
 import { getGraph } from "../actions";
 import { useTheme } from "next-themes";
 
 const ForceGraphComponent = () => {
-  const fgRef = useRef();
+  const fgRef = useRef<any>();
   const [graph, setGraph] = useState(null);
   const { theme } = useTheme();
+
+  const handleClick = useCallback(
+    (node: { x: number; y: number; z: number }) => {
+      const distance = 40;
+      const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+      fgRef.current?.cameraPosition(
+        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio },
+        node,
+        3000,
+      );
+    },
+    [fgRef],
+  );
 
   useEffect(() => {
     const fetchGraph = async () => {
@@ -31,6 +44,8 @@ const ForceGraphComponent = () => {
           graphData={graph!}
           nodeLabel="description"
           nodeAutoColorBy="group"
+          onNodeClick={handleClick}
+          showNavInfo={false}
         />
       )}
     </div>
