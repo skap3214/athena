cbf7466b947bfec842ed64cc872ffbb04ff55ee7
skap3214 <kapadiasoami@gmail.com>
2024-04-10 13:46:26 -0400

    add to updateGraph function

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -1,49 +1,54 @@
 "use server";
 import { Document } from "langchain/document";
-import { extractRelations, loadFromText, loadFromYoutubeLink } from "./extract";
+import { extractRelations, loadFromText, loadFromYoutubeLink, loadFromPDF } from "./extract";
 import { insertRelations } from "./insert";
 import { getAllEdges, getAllNodes } from "@/supabase/actions";
 import { Node, Edge } from "@/types";
 
-export async function updateGraph(text?: string, url?: string): Promise<void> {
-  // Validate input
-  if ((!text && !url) || (text && url)) {
-    throw new Error("Either text OR url should be given");
-  }
+export async function updateGraph(text?: string, url?: string, file?: File): Promise<void> {
+    // Validate input
+    if ((!text && !url && !file) || (text && url && file)) {
+        throw new Error("Either text OR url OR file should be given");
+    }
 
-  // Convert to documents
-  let documents: Document[];
-  if (url) {
-    documents = await loadFromYoutubeLink(url);
-  } else {
-    documents = await loadFromText(text!);
-  }
+    // Convert to documents
+    let documents: Document[];
 
-  // Extract relations
-  const relations = await extractRelations(documents);
+    if (file) {
+        documents = await loadFromPDF(file);
+    } else {
+        if (url) {
+            documents = await loadFromYoutubeLink(url);
+        } else {
+            documents = await loadFromText(text!);
+        }
+    }
 
-  // Insert relations
-  return insertRelations(documents, relations);
+    // Extract relations
+    const relations = await extractRelations(documents);
+
+    // Insert relations
+    return insertRelations(documents, relations);
 }
 
 export async function getGraph(): Promise<any> {
-  const edges = await getAllEdges();
-  const nodes = await getAllNodes();
+    const edges = await getAllEdges();
+    const nodes = await getAllNodes();
 
-  const formattedNodes = nodes.map((node: Node) => ({
-    id: node.id,
-    description: node.data,
-  }));
+    const formattedNodes = nodes.map((node: Node) => ({
+        id: node.id,
+        description: node.data,
+    }));
 
-  const formattedEdges = edges.map((edge: Edge) => ({
-    source: edge.from,
-    target: edge.to,
-    content: edge.data,
-  }));
+    const formattedEdges = edges.map((edge: Edge) => ({
+        source: edge.from,
+        target: edge.to,
+        content: edge.data,
+    }));
 
-  const graphData = {
-    nodes: formattedNodes,
-    links: formattedEdges,
-  };
-  return graphData;
+    const graphData = {
+        nodes: formattedNodes,
+        links: formattedEdges,
+    };
+    return graphData;
 }
