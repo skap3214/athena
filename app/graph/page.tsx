09996ff960047227777c633fcc906e9a6f72d4ac
achyutbenz19 <achyut.benz@gmail.com>
2024-04-10 20:10:04 -0400

    build

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -1,54 +1,59 @@
 "use client";
+import { useCallback, useEffect, useRef, useState } from "react";
 import dynamic from "next/dynamic";
-import { useRef, useState, useCallback, useEffect } from "react";
 import { getGraph } from "../actions";
 import { useTheme } from "next-themes";
 
-const ForceGraph3D = dynamic(
-  () => import("react-force-graph").then((mod) => mod.ForceGraph3D),
-  {
-    ssr: false,
-  },
-);
+const ForceGraph3D = dynamic(() => import("react-force-graph").then(mod => mod.ForceGraph3D), {
+  ssr: false,
+});
 
 const ForceGraphComponent = () => {
-  const fgRef = useRef<any>();
+  const fgRef = useRef<any>(null)
   const [graph, setGraph] = useState(null);
   const { theme } = useTheme();
+  const [isComponentReady, setIsComponentReady] = useState(false);
 
-  const handleClick = useCallback(
-    (node: any) => {
-      const distance = 40;
-      const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
-      fgRef.current?.cameraPosition(
-        { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio },
-        node,
-        3000,
-      );
-    },
-    [fgRef],
-  );
-
+  // Fetch graph data
   useEffect(() => {
     const fetchGraph = async () => {
       try {
         const graphData = await getGraph();
         setGraph(graphData);
       } catch (error) {
-        // console.error('Error fetching graph data:', error);
+        console.error('Error fetching graph data:', error);
       }
     };
 
     fetchGraph();
   }, []);
 
+  const handleClick = useCallback((node: any) => {
+    if (!fgRef) {
+      console.log("ForceGraph3D ref is not set.");
+      return;
+    }
+    const distance = 40;
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+
+    fgRef.current?.cameraPosition(
+      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio },
+      node,
+      3000
+    );
+  }, [fgRef]);
+
+  useEffect(() => {
+    setIsComponentReady(true);
+  }, []);
+
   return (
     <div className="max-h-screen">
-      {graph && (
+      {graph && isComponentReady && (
         <ForceGraph3D
-          ref={fgRef}
+          ref={fgRef} // Set the ref directly.
           backgroundColor={theme === "light" ? "#FFFF" : "#0A0A0A"}
-          graphData={graph!}
+          graphData={graph}
           nodeLabel="description"
           nodeAutoColorBy="id"
           onNodeClick={handleClick}
